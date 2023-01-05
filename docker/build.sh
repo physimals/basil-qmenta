d360126f36f5da4b30cf4c23733c517b78543f33
Martin Craig <martin.craig@nottingham.ac.uk>
2023-01-05 11:51:56 +0000

    Add version tagging

--- docker/build.sh
+++ docker/build.sh
@@ -3,4 +3,5 @@
 tag=`cat version.txt`
 docker build -t martincraig/basil-qmenta .
 docker tag martincraig/basil-qmenta martincraig/basil-qmenta:$tag 
-#docker push martincraig/basil-qmenta:$tag
+docker push martincraig/basil-qmenta:$tag
+docker push martincraig/basil-qmenta:latest
